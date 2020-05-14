00af3cc8f98d87bb3ccd835e91a4f6566425ff98
Tristan Langford <tristanlangford@hotmail.com>
2020-05-14 14:53:23 +0100

    making DockStation release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,13 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'release bike creates a bike instance' do
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
   end
+
+it 'expects Bike to be working' do
+  expect(Bike.new.working).to eq(true)
+end
+
+end
