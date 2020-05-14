addc57bf20bc5c98304111cf36a7990a1f4c1076
Tristan Langford <tristanlangford@hotmail.com>
2020-05-14 16:36:23 +0100

    Wrapping Collections

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,18 @@
 class DockingStation
   attr_reader :abike
+
+def initialize
+  @bikes = []
+end
+
   def release_bike
-    fail 'No Bike Docked' if @abike.nil?
-    @abike
+    fail 'No Bike Docked' if @bikes.empty?
+    @bikes
   end
 
   def dock(bike)
-    fail 'Dock full' if @abike != nil
-    @abike = bike
+    fail 'Dock full' if @bikes.length > 19
+    @bikes << bike
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,16 +12,16 @@ it 'expects Bike to be working' do
 end
 
 it 'allows a bike to be docked' do
-  expect(DockingStation.new.dock("abike")).to eq("abike")
+  expect(DockingStation.new.dock("abike")).to include("abike")
 end
 
-  it 'raise error when @abike is nil' do
+  it 'raise error when @bikes is nil' do
     expect{DockingStation.new.release_bike}.to raise_error('No Bike Docked')
   end
 
 it 'raises an error when docking station is full' do
   docking = DockingStation.new
-  docking.dock("bike")
+  20.times { docking.dock("bike") }
   expect{docking.dock("bike")}.to raise_error('Dock full')
 end
 
